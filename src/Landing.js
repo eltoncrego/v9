7b716e20347b15d84456f50a0bca973b4cf82897
Elton C. Rego <rego.elton@gmail.com>
2018-03-15 12:35:23 -0700

    Added basic navigation bar

--- src/Landing.js
+++ src/Landing.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './Landing.css';
 import './index.css';
 import logo from './assets//svg/logo/light.svg'
 
 class Landing extends Component {
+
+  componentDidMount(){
+  }
+
   render() {
     return(
       <div class="landing">
         <div class="navbar">
-          <img src={logo} class="site-logo"></img>
+          <img src={logo} class="site-logo" onClick={() => ReactDOM.render(
+            <Landing/>,
+            document.getElementById('root')
+          )}/>
+        <div class="nav-links">
+          <ul>
+            <li><a href="#" class="light-subheader-secondary">about me</a></li>
+            <li><a href="#" class="light-subheader-secondary">resume</a></li>
+            <li><a href="#" class="light-subheader-secondary">projects</a></li>
+          </ul>
+        </div>
         </div>
         <div class="content">
           <p class="light-display2">Welcome to my space!</p>
