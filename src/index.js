61604d2105028715802ce3afbb7de292df46bc20
Elton C. Rego <rego.elton@gmail.com>
2018-03-15 16:16:53 -0700

    Added basic fade in animation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Landing from './Landing';
+import Landing from './pages/Landing';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Landing/>, document.getElementById('root'));
