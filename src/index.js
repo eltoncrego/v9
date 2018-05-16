a6a2688154f4b37b28255c8fd7433f634dc8f165
Elton C. Rego <rego.elton@gmail.com>
2018-05-16 11:32:35 -0700

    Starting from scratch

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Landing from './pages/Landing';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Landing/>, document.getElementById('root'));
-registerServiceWorker();
