13a9ff0c5192dfbafd50ad97382b4959e27fc1a8
Elton C. Rego <rego.elton@gmail.com>
2019-10-12 15:57:11 -0700

    Add working navbar system

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,6 +1,31 @@
 var appname = angular.module('eltoncrego', []);
 appname.controller('siteController', ['$scope',
   function($scope) {
+    $scope.hamburgerState = "";
+    $scope.navbarState = "";
     $scope.title = "Elton C. Rego"
+    $scope.toggleHamburger = function(){
+      if ($scope.hamburgerState === "") {
+        $scope.hamburgerState = "is-active";
+        $scope.navbarState = "navbar-active";
+      } else {
+        $scope.hamburgerState = "";
+        $scope.navbarState = "";
+      }
+    }
   }
 ]);
+
+jQuery(document).ready(function($) {
+  var alterClass = function() {
+    var ww = document.body.clientWidth;
+    if (ww >= 768) {
+      $('.navbar').removeClass('navbar-active');
+      $('.site-logo').removeClass('navbar-active');
+      $('.hamburger').removeClass('is-active');
+    }
+  };
+  $(window).resize(function(){
+    alterClass();
+  });
+});
