b17451d4019f071ffc193ad7385ee27e952f0e98
Elton C. Rego <rego.elton@gmail.com>
2020-04-10 14:55:48 -0700

    Set up message constants

---
+++ src/constants/messages.js
@@ -0,0 +1,11 @@
+const MESSAGES = {
+  LANDING: {
+    WAVE_ICON: "👋🏽",
+    LEAD_IN: "Hello there! I'm",
+    TITLE: "Elton Christopher Rego.",
+    SUB_TITLE: "I make stuff on the web look great.",
+    DESC: "As a software engineer based in the Oakland, California, I specialize in implementing beautiful UI designs and occasionally doing the design work myself.",
+  }
+};
+
+export default MESSAGES;
