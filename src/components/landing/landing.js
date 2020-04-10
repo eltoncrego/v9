e63be95d7a5a14c19a7fbe031283012060209e20
Elton C. Rego <rego.elton@gmail.com>
2020-04-10 16:24:42 -0700

    Modify copy and address layout issues

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -7,13 +7,15 @@ const Landing = () => {
   const LANDING = MESSAGES.LANDING;
   return (
     <div className='landing'>
-      <div className='landing__lead-in'>
-        <div id='lead-in__wave-icon' className='text--lead'>{LANDING.WAVE_ICON}</div>
-        <div className='text--lead'>{LANDING.LEAD_IN}</div>
+      <div className='landing__text'>
+        <div className='landing__lead-in'>
+          <div id='lead-in__wave-icon' className='text--lead'>{LANDING.WAVE_ICON}</div>
+          <div className='text--lead'>{LANDING.LEAD_IN}</div>
+        </div>
+        <div className='text--uber'><b>{LANDING.TITLE}</b></div>
+        <h2 className='text--grey'>{LANDING.SUB_TITLE}</h2>
+        <div className='text--body'>{LANDING.DESC}</div>
       </div>
-      <div className='text--uber'><b>{LANDING.TITLE}</b></div>
-      <h2 className='text--grey'>{LANDING.SUB_TITLE}</h2>
-      <div className='text--body'>{LANDING.DESC}</div>
     </div>
   );
 };
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,7 +4,7 @@ const MESSAGES = {
     LEAD_IN: "Hello there! I'm",
     TITLE: "Elton Christopher Rego.",
     SUB_TITLE: "I make stuff on the web look great.",
-    DESC: "As a software engineer based in the Oakland, California, I specialize in implementing beautiful UI designs and occasionally doing the design work myself.",
+    DESC: "As a software engineer based in Oakland, California, I specialize in implementing beautiful UI designs and occasionally doing the design work myself.",
   }
 };
 
