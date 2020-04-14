30eddba5f66daadeab225cb2b5f4cb3f2bccc804
Elton C. Rego <rego.elton@gmail.com>
2020-04-13 19:03:27 -0700

    Built out basic scroll function for landing

---
+++ src/components/about/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './about.css';
+
+const About = () => {
+  return(
+    <div id='about'>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+export default About;
--- src/components/site.js
+++ src/components/site.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './nav/nav';
 import Landing from './landing/landing';
+import About from './about/about';
 
 const Site = () => {
   return (
     <div>
       <Nav></Nav>
-      <Landing></Landing>
+      <Landing nextSection='about'></Landing>
+      <About></About>
     </div>
   );
 };
