2905d3858480ba5828600ae4887fb4c742f8852f
Elton C. Rego <rego.elton@gmail.com>
2020-04-14 11:19:16 -0700

    Add a CTA button

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
 import MESSAGES from './../../constants/messages';
+import Button from '../shared/button';
 import './landing.css';
 
 const Landing = () => {
-  const { LANDING } = MESSAGES;
+  const { LANDING, LINKS } = MESSAGES;
   return (
     <div id='landing'>
       <div className='landing__hero'>
         <h1 className='landing__lead-in'>{LANDING.LEAD_IN}</h1>
         <h2 className='landing__title'>{LANDING.TITLE}</h2>
         <h2 className='landing__subtitle'>{LANDING.SUB_TITLE} 
-          <a href="https://www.gantri.com/">{LANDING.COMPANY_LINK}.</a>
+          <a href={LINKS.GANTRI}>{LANDING.COMPANY_LINK}.</a>
         </h2>
         <span className='landing__desc'>{LANDING.DESC}</span>
+        <Button 
+          text={LANDING.CTA_BUTTON}
+          onClickAction={() => window.open(LINKS.EMAIL)}
+        />
       </div>
     </div>
   );
--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,16 +1,23 @@
 const MESSAGES = {
+  LINKS: {
+    GANTRI: 'https://www.gantri.com/',
+    EMAIL: 'mailto:rego.elton@gmail.com'
+  },
   LANDING: {
     LEAD_IN: "Hi there! I'm",
     TITLE: "Elton Christopher Rego,",
     SUB_TITLE: "a Front-End Engineer ",
     COMPANY_LINK: "@Gantri",
     DESC: "I'm based in Oakland, California and specialize in developing, implementing, and occasionally designing exceptional User Interface designs on the Web.",
-    CTA_BUTTON: "Let's get in touch."
+    CTA_BUTTON: "Let's get in touch!"
   },
   ABOUT: {
     TITLE: "About Me:",
     DESC: "I graduated from the University of California, Santa Cruz in 2018 with degrees in Computer Science and Business Management Economics and honors in the major.",
     DESC_2: "Out of college, I joined a small company, Veeva, in Pleasanton that is aiming to build the industry cloud for life sciences.",
+  },
+  DEFAULTS: {
+    BUTTON_TEXT: 'do the thing!',
   }
 };
 
