faf089b92ac88e3a7bfde87fc0cb214e7415df46
Elton C. Rego <rego.elton@gmail.com>
2020-04-15 09:44:38 -0700

    Create an initial object for the resume components

---
+++ src/components/resume/option/option.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './option.css';
+
+const Option = ({ option, onClickAction, isActive }) => {
+  return (
+    <div className={`option ${isActive ? 'option--active': ''}`} onClick={() => onClickAction()}>
+      <h1>{option.title} <a href={option.site_link}>@{option.company}.</a></h1>
+      <div className='option__text--secondary'>{option.time}</div>
+      <div className='option__text--secondary'>{option.location}</div>
+    </div>
+  )
+};
+
+export default Option;
--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import RESUME from '../../constants/content--resume';
+import Option from './option/option';
 import './../shared/styles/card-grid.css';
 import './resume.css';
 
 const Resume = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const resumeCopy = RESUME.slice();
+  let resumeOptions = resumeCopy.map((job, index) => {
+    return(
+      <Option 
+        onClickAction={() => setActiveIndex(index)} 
+        key={index} 
+        option={job} 
+        isActive={index === activeIndex}/>
+    );
+  });
+
+  const resumeContent = ({ desc }) => {
+    return (
+      <div>{desc}</div>
+    );
+  }
+
   return (
     <div id='resume' className='home-page__card'>
-      <div class='card__child--whole-width resume__options'>
+      <div className='card__child--whole-width resume__options'>
+        {resumeOptions}
       </div>
-      <div class='card__child--content-padding resume__content'>
+      <div className='card__child--content-padding resume__content'>
+        {resumeContent(resumeCopy[activeIndex])}
       </div>
     </div>
   )
