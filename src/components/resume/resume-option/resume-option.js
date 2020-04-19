d17b7739199854f9ce7bb08b443c16b47e313865
Elton C. Rego <rego.elton@gmail.com>
2020-04-19 13:41:02 -0700

    Switch to google font so that I dont have to worry about packing woff files

--- src/components/resume/resume-option/resume-option.js
+++ src/components/resume/resume-option/resume-option.js
@@ -5,7 +5,7 @@ import './resume-option.css';
 const Option = ({ option, onClickAction, isActive }) => {
   return (
     <div className={`option ${isActive ? 'option--active': ''}`} onClick={() => onClickAction()}>
-      <h1>{option.title} <a href={option.site_link}>@{option.company}.</a></h1>
+      <h1>{option.title} <a className='option__company-link' href={option.site_link}>@{option.company}.</a></h1>
       <div className='option__text--secondary'>{option.time}</div>
       <div className='option__text--secondary'>{option.location}</div>
     </div>
