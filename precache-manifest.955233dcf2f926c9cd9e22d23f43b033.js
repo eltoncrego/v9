7ec34c16b8dbd517ed9451f17fcf7cc4828940d6
Elton C. Rego <rego.elton@gmail.com>
2020-04-24 16:27:33 -0700

    Updates

---
+++ precache-manifest.955233dcf2f926c9cd9e22d23f43b033.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a36c4a33dc4ed7ef3169db6d3e52b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e4562c56d02f1bed229",
+    "url": "/static/css/main.2447099a.chunk.css"
+  },
+  {
+    "revision": "89520b6877fa9f27a931",
+    "url": "/static/js/2.7d629634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d629634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4562c56d02f1bed229",
+    "url": "/static/js/main.64e731ae.chunk.js"
+  },
+  {
+    "revision": "97920cb1bedc40fb1ef2",
+    "url": "/static/js/runtime-main.b5ac5d84.js"
+  }
+]);
