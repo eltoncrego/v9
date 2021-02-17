276b37a5bfbfbd4bcd67aa4f1c6c1a46fbf9bb01
Elton C. Rego <rego.elton@gmail.com>
2021-02-17 14:34:17 -0800

    Updates

---
+++ precache-manifest.9db903d73ee9ac7dbc5668a3f1a7e374.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9331a4f8bd01b354b1ae10c25c6f1c71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "364af833b2ea070fa475",
+    "url": "/static/css/main.b27726bc.chunk.css"
+  },
+  {
+    "revision": "f1e1a11278219aa323b7",
+    "url": "/static/js/2.e4235c33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e4235c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364af833b2ea070fa475",
+    "url": "/static/js/main.15782811.chunk.js"
+  },
+  {
+    "revision": "da3a610b843a9fd9b419",
+    "url": "/static/js/runtime-main.bb6a9f36.js"
+  },
+  {
+    "revision": "1ced22ee6d45bdd05a5383fd9dbd1b43",
+    "url": "/static/media/CircularStd-Bold.1ced22ee.woff"
+  },
+  {
+    "revision": "ed76eb21560f0dc038cce0adfd65ea3c",
+    "url": "/static/media/CircularStd-Book.ed76eb21.woff"
+  }
+]);
