742075b58e59a4beff653285f10f613aa3e92535
Elton C. Rego <rego.elton@gmail.com>
2021-02-17 15:13:52 -0800

    Scrap everything to start over

--- src/components/loader/loader-context/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const LoaderContext = React.createContext(null);
-
-export default LoaderContext;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Site from './components/site';
-import './styles.css';
-
-ReactDOM.render(<Site/>, document.getElementById('elton-site__root'));
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('elton-site__root'));
